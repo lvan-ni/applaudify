150d3f0923f37e312ad26a263a5086124d7d328b
lvan-ni <g.nilvan@icloud.com>
2024-03-14 11:22:14 +0100

    Initial set up for the new art directions and UI

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,28 +5,14 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        black: '#000000',
-        metal: '#0d0d0d',
-        charcoal: '#303030',
-        stone: '#5a5a5a',
-        hdrln: 'rgba(178, 178, 178, 0.856)',
-        gray: '#919191',
-        silver: '#b8b8b8',
-        light: '#f6f6f6',
-        paper: '#f8f6f2',
-        white: '#ffffff',
-        blue: '#3db3e2',
-        hdr: '#f6f6f67a',
-      },
-      backgroundImage: {
-        logo: 'linear-gradient(125deg, #3db3e2 16.46%, #9ED7F3 36.98%, #D7BDF5 51.69%, #CC81F1 60.85%, #E96069 71.67%, #F09674 91.21%)',
+        sunrise: '#FF4B11',
+        day: '#ffffff',
       },
       fontFamily: {
         josefin: ['Josefin Sans', 'sans-serif'],
         avenir: ['Avenir Next', 'sans-serif'],
       },
       fontSize: {
-        '8': '8px',
         '10': '10px',
         '12': '12px',
         '14': '14px',
@@ -35,22 +21,37 @@ const config: Config = {
         '20': '20px',
         '24': '24px',
         '30': '30px',
-        '36': '36px',
+        '40': '40px',
         '60': '60px',
+        '80': '80px',
+        '100': '100px',
+        '140': '140px',
       },
-      fontWeight: {
-        400: '400',
-        500: '500',
-        600: '600',
-        700: '700',
-      },
-      backgroundOpacity: {
-        '10': '0.1',
-        '25': '0.25',
-        '50': '0.5',
-      },
-      backdropBlur: {
-        xs: '2px',
+      screens: {
+        // default 640px
+        // iPhone SE Portrait
+        phoneSEP: '320px',
+
+        // iPhone 14/15 Pro Portrait
+        phoneP: '393px',
+
+        // iPad Pro 11" Portrait
+        tabletP: '834px',
+
+        // iPhone SE Landscape
+        phoneSEL: '568px',
+
+        // iPhone 14/15 Pro Landscape
+        phoneL: '852px',
+
+        // iPad Pro 11" Landscape
+        tabletL: '1194',
+
+        // MacBook Pro 14" Landscape
+        laptopL: '1512px',
+
+        // Studio Display 27" Landscape
+        desktopL: '2560px',
       },
     },
   },
